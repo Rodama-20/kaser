f5a918c75c7e9c1c6489d81aa08f7e81724ee0c4
Cyrille Polier <rodama20@gmail.com>
2024-03-10 20:42:55 +0100

    feat: add media and admin

--- api/kaser/admin.py
+++ api/kaser/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Photo, Album
+
 # Register your models here.
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("title", "album", "date_uploaded")
+    search_fields = ("title", "album__title")
+    list_filter = ("album", "date_uploaded")
+    
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_created")
+    search_fields = ("title", "date_created")
+    list_filter = ("date_created",)
+    
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Album, AlbumAdmin)
